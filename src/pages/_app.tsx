cae520323f743c0bd17f4bb68ff8960b418ee027
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-14 23:50:04 +0900

    ✨

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { MantineProvider } from "@mantine/core";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "dark",
+        fontFamily: "Verdana, sans-serif",
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
