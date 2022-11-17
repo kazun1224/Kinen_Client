52e1e735440c040a90e3eb1d9e286a650c783fba
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-17 22:26:38 +0900

    layout componentsの作成

---
+++ src/layouts/layout.tsx
@@ -0,0 +1,14 @@
+import { CustomLayout } from "next";
+import Head from "next/head";
+
+export const Layout: CustomLayout = (props) => {
+  return (
+    <>
+      <Head>
+        <title>Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="py-20 md:py-32">{props.children}</main>
+    </>
+  );
+};
