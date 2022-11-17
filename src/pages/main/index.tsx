a2510fa2d264b7c6e54cea8395efcf776cb26e70
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-17 23:09:43 +0900

    logout機能の実装

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,8 +1,17 @@
+import { Button } from '@mantine/core'
+import axios from 'axios'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const Main = () => {
+  const router = useRouter();
+  const logout = async () => {
+    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`)
+    router.push('/')
+  }
+
   return (
-    <div>Main</div>
+    <div><Button onClick={logout}>logout</Button></div>
   )
 }
 
