6665ba7b48c24c826053deae5c7266d010c0cb13
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-17 22:27:31 +0900

    マウント時にcsrf-tokenを取得する処理を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,35 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { useEffect } from "react";
-import { MantineProvider } from "@mantine/core";
+import 'src/styles/globals.css'
+import type { AppProps, CustomAppPage } from 'next/app'
+import { useEffect } from 'react'
+import { MantineProvider } from '@mantine/core'
+import { useLoading } from '../hooks/useLoading'
+import axios from 'axios'
 
-export default function App({ Component, pageProps }: AppProps) {
+const MyApp: CustomAppPage = ({ Component, pageProps }) => {
+  const { pageLoading, loadingComponent } = useLoading()
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  axios.defaults.withCredentials = true;
+
+  useEffect(()=> {
+    const getCsrfToken = async () => {
+      const { data} = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/auth/csrf`);
+      axios.defaults.headers.common["csrf-token"] = data.csrfToken;
+    }
+    getCsrfToken();
+  })
   return (
     <MantineProvider
       withGlobalStyles
       withNormalizeCSS
       theme={{
-        colorScheme: "dark",
-        fontFamily: "Verdana, sans-serif",
+        colorScheme: 'dark',
+        fontFamily: 'Verdana, sans-serif',
       }}
     >
-      <Component {...pageProps} />
+      {pageLoading ? loadingComponent : getLayout(<Component {...pageProps} />)}
     </MantineProvider>
-  );
+  )
 }
+
+export default MyApp
