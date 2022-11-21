6b55e226d8f4c27c6f5ffe61a6c214b8582f95bc
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-21 23:59:31 +0900

    ページ遷移時のローディングコンポーネントを作成

--- src/hooks/useLoading.tsx
+++ src/hooks/useLoading.tsx
@@ -1,29 +1,28 @@
-
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { Loading } from "src/components/Loading";
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { Loading } from 'src/components/Loading'
 
 export const useLoading = () => {
   // loadingのロジック
-  const router = useRouter();
-  const [pageLoading, setPageLoading] = useState(false);
+  const router = useRouter()
+  const [pageLoading, setPageLoading] = useState(false)
 
   useEffect(() => {
     const handleStart = (url: any) =>
-      url !== router.asPath && setPageLoading(true);
-    const handleComplete = () => setPageLoading(false);
+      url !== router.asPath && setPageLoading(true)
+    const handleComplete = () => setPageLoading(false)
 
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
 
     return () => {
-      router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleComplete);
-      router.events.off("routeChangeError", handleComplete);
-    };
-  });
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  })
 
-  const loadingComponent = <Loading />;
-  return { pageLoading, loadingComponent };
-};
+  const loadingComponent = <Loading />
+  return { pageLoading, loadingComponent }
+}
