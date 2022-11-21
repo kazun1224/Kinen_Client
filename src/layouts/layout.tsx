a82168c0570f9e271f9ad876a7bb9220739456d3
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-21 23:57:25 +0900

    Layoutの作成

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,14 +1,18 @@
 import { CustomLayout } from "next";
 import Head from "next/head";
+import { HeaderComponent } from "src/layouts/header";
+import { FooterComponent } from "src/layouts/footer";
 
 export const Layout: CustomLayout = (props) => {
   return (
     <>
       <Head>
-        <title>Portfolio</title>
+        <title>Kinen</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <HeaderComponent/>
       <main className="py-20 md:py-32">{props.children}</main>
+      <FooterComponent/>
     </>
   );
 };
