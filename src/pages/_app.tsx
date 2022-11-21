94d824950adc5f96aa89fc20499895ec17dce439
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-21 23:56:34 +0900

    react queryの初期化

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,31 +4,47 @@ import { useEffect } from 'react'
 import { MantineProvider } from '@mantine/core'
 import { useLoading } from '../hooks/useLoading'
 import axios from 'axios'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 const MyApp: CustomAppPage = ({ Component, pageProps }) => {
   const { pageLoading, loadingComponent } = useLoading()
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  axios.defaults.withCredentials = true;
+  axios.defaults.withCredentials = true
 
-  useEffect(()=> {
+  useEffect(() => {
     const getCsrfToken = async () => {
-      const { data} = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/auth/csrf`);
-      axios.defaults.headers.common["csrf-token"] = data.csrfToken;
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/csrf`
+      )
+      axios.defaults.headers.common['csrf-token'] = data.csrfToken
     }
-    getCsrfToken();
-  })
+    getCsrfToken()
+  }, [])
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: 'dark',
-        fontFamily: 'Verdana, sans-serif',
-      }}
-    >
-      {pageLoading ? loadingComponent : getLayout(<Component {...pageProps} />)}
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+          fontFamily: 'Verdana, sans-serif',
+        }}
+      >
+        {pageLoading
+          ? loadingComponent
+          : getLayout(<Component {...pageProps} />)}
+      </MantineProvider>
+    </QueryClientProvider>
   )
 }
 
