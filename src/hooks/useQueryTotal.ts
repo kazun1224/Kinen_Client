cab097a6ce7b45b030f5f00dbeb886ac668107b6
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-28 20:43:26 +0900

    金額のデータを取得するcustom hooksを作成

---
+++ src/hooks/useQueryTotal.ts
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+
+export const useQueryTotal = () => {
+  const router = useRouter()
+
+  const getTotal = async () => {
+    const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/total`)
+    return data
+  }
+
+  return useQuery({
+    queryKey: ['total'],
+    queryFn: getTotal,
+    onError: (err: any) => {
+      if (err.response.status === 401 || err.response.status === 403) {
+        router.push('/')
+      }
+    },
+  })
+}
