fbf9f064b532cee354f8d8589ba0a68f2a3500a5
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-28 20:44:14 +0900

    金額のデータを上書きするcustom hookを作成

---
+++ src/hooks/useMutateTotal.ts
@@ -0,0 +1,40 @@
+import { Total } from '@prisma/client'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+
+export const useMutateTotal = () => {
+  const queryClient = useQueryClient()
+  const router = useRouter()
+
+  const updateTotalMutation = useMutation(
+    async (cigaretteId: string) => {
+      const res = await axios.patch(
+        `${process.env.NEXT_PUBLIC_API_URL}/total/cigarette`,
+        cigaretteId
+      )
+      return res.data
+    },
+    {
+      onSuccess: (res) => {
+        const previousTotal = queryClient.getQueryData<Total>([
+          'total',
+        ])
+        if (previousTotal) {
+          queryClient.setQueryData(
+            ['total'],
+            res
+          )
+        }
+      },
+      onError: (err: any) => {
+        if (err.response.status === 401 || err.response.status === 403) {
+          router.push('/')
+        }
+      },
+    }
+  )
+
+  return updateTotalMutation;
+}
+
