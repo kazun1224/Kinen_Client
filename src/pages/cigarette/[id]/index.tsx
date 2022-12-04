71eb87dd3a111b3832f51d389b7531e71538f884
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-12-04 18:26:34 +0900

    編集用ページへのリンクを作成

--- src/pages/cigarette/[id]/index.tsx
+++ src/pages/cigarette/[id]/index.tsx
@@ -1,6 +1,7 @@
 import { Text } from '@mantine/core'
 import { Cigarette } from '@prisma/client'
 import { CustomNextPage } from 'next'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useQueryCigaretteById } from 'src/hooks/useQueryCigarettes'
 import { useQueryUser } from 'src/hooks/useQueryUser'
@@ -10,6 +11,7 @@ const CigaretteForm: CustomNextPage = () => {
   const router = useRouter()
 
   const { data: cigarette, status } = useQueryCigaretteById(router.query.id)
+  console.log(cigarette)
 
   if (status === 'loading') {
     return <Text>Loading...</Text>
@@ -19,7 +21,12 @@ const CigaretteForm: CustomNextPage = () => {
     return <Text>データの取得に失敗しました。</Text>
   }
 
-  return <div>{cigarette.name}</div>
+  return (
+    <div>
+      <h1>{cigarette.name}</h1>
+      <Link href={`${router.query.id}/edit`}>編集する</Link>
+    </div>
+  )
 }
 
 CigaretteForm.getLayout = (page) => <Layout>{page}</Layout>
